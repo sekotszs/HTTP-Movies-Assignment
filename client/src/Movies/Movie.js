a888d43b4cffa716ea889d8b90c4003cdc6edc7f
Zoe Stokes <sekotszs@gmail.com>
2020-04-22 14:12:24 -0700

    started

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const history = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -25,7 +26,10 @@ function Movie({ addToSavedList }) {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
+ const handleEdit = () => {
+history.push(`/update-movie/${movie.id}`)
 
+ }
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
@@ -33,8 +37,10 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+     <button onClick={handleEdit}>Update</button>
     </div>
   );
 }
 
 export default Movie;
+//<button className="md-button form-button">Update</button>
